54df5a52707a5c2307f905c7f095a08a34657939
Pakhi Shukla <143419104+Pakhi-308@users.noreply.github.com>
2026-04-11 00:07:23 +0530

    Delete OI File Manager CloudDemo directory

--- OI File Manager CloudDemo/src/org/openintents/filemanager/demo/Dropboxconfig.java
+++
@@ -1,8 +0,0 @@
-package org.openintents.filemanager.demo;
-
-public class Dropboxconfig {
-
-	public static final String appKey = "ijcxja4c41evhxt";
-	
-	public static final String appSecret = "rre5pkobmp7ki7v";
-}
